e76892efda7af5cc8e91bea1f22788317367a5c9
MinexAutomation <minex.automation@gmail.com>
2019-12-16 12:51:56 -0800

    Added C# project file extension.

--- source/R5T.Pickwich.Types/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Pickwich.Types
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Pickwich.Types/Code/Classes/FileExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Pickwich.Types
+{
+    public static class FileExtensions
+    {
+        public static readonly string CSharpProjectFileExtension = "csproj";
+    }
+}
