bf78adf3aed915bf70f81a818513f7d4743b669f
MinexAutomation <minex.automation@gmail.com>
2019-12-16 13:02:13 -0800

    Added project types enumeration.

---
+++ source/R5T.Pickwich.Types/Code/Enumerations/ProjectType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace R5T.Pickwich.Types
+{
+    /// <summary>
+    /// Project types.
+    /// </summary>
+    public enum ProjectType
+    {
+        Library = 0,
+        Console,
+        Test,
+    }
+}
